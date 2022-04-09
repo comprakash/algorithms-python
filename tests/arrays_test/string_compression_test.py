58ad702d0b980948d0bd15bd06c086512d33a6b5
Om Prakash C <comprakash@gmail.com>
2022-04-09 08:59:35 +0530

    Added String compression code

---
+++ tests/arrays_test/string_compression_test.py
@@ -0,0 +1,10 @@
+from arrays.string_compression import compress_repeating_letters
+
+
+def test_compress_repeating_letters():
+    assert(compress_repeating_letters("") == "")
+    assert(compress_repeating_letters("AABBCC") == "A2B2C2")
+    assert(compress_repeating_letters("AAABCCDDDDD") == "A3B1C2D5")
+    assert(compress_repeating_letters("AAAaaa") == "A3a3")
+    assert(compress_repeating_letters("AAB") == "A2B1")
+    assert(compress_repeating_letters("AAAABBBBCCCCCDDEEEE") == "A4B4C5D2E4")
